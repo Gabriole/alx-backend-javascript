99c4928cc07dacce23fe46cdeab98375eb81ef7d
Gabriol <ismail.mouhtaram@gmail.com>
2024-10-02 08:52:13 +0000

    Java1

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,17 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const user = await signUpUser(firstName, lastName);
-    const photo = await uploadPhoto(fileName);
-    return {
-      user,
-      photo,
-    };
-  } catch (error) {
-    return {
-      user: null,
-      photo: null,
-    };
-  }
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 }
